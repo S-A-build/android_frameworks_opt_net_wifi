2858a5c338c081702c93b89936f937fe0a1ca46e
Renlord <me@renlord.com>
2019-12-21 00:33:56 +1100

    add support for always generating new random MAC
    To trigger re-generation of randomized MAC addressed for an already
    connected AP. User simply has to toggle on/off wifi. Otherwise, on
    re-connection, a new randomized MAC address also gets generated.
    
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -3455,7 +3455,12 @@ public class ClientModeImpl extends StateMachine {
         String currentMacString = mWifiNative.getMacAddress(mInterfaceName);
         MacAddress currentMac = currentMacString == null ? null :
                 MacAddress.fromString(currentMacString);
-        MacAddress newMac = config.getOrCreateRandomizedMacAddress();
+        MacAddress newMac;
+        if (config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_PERSISTENT) {
+            newMac = config.getOrCreateRandomizedMacAddress();
+        } else {
+            newMac = MacAddress.createRandomUnicastAddress();
+        }
         mWifiConfigManager.setNetworkRandomizedMacAddress(config.networkId, newMac);
         if (!WifiConfiguration.isValidMacAddressForRandomization(newMac)) {
             Log.wtf(TAG, "Config generated an invalid MAC address");
@@ -4367,7 +4372,7 @@ public class ClientModeImpl extends StateMachine {
                     reportConnectionAttemptStart(config, mTargetRoamBSSID,
                             WifiMetricsProto.ConnectionEvent.ROAM_UNRELATED);
                     if (config.macRandomizationSetting
-                            == WifiConfiguration.RANDOMIZATION_PERSISTENT
+                            != WifiConfiguration.RANDOMIZATION_NONE
                             && mConnectedMacRandomzationSupported) {
                         configureRandomizedMacAddress(config);
                     } else {
